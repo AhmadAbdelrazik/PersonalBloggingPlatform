183694ad175efa1ad2a72d44692c0be291489d8b
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-06-17 17:10:36 +0300

    Added async handler to handle errors

--- router/posts.js
+++ router/posts.js
@@ -3,23 +3,24 @@ const router = express.Router();
 const postValidate = require('../Middleware/validator');
 const params = require('../Middleware/Params');
 const controller = require('../controller/posts');
+const asyncHandler = require('../utils/asyncHandler')
 
 // id parameter
 router.param('id', params.id);
 
 // Add a post
-router.post('/',postValidate, controller.addPost);
+router.post('/',postValidate, asyncHandler(controller.addPost));
 
 // Read the latest posts
-router.get('/', controller.readPosts);
+router.get('/', asyncHandler(controller.readPosts));
 
 // Read a post
-router.get('/:id', controller.readPost);
+router.get('/:id', asyncHandler(controller.readPost));
 
 // Change a post
-router.put('/:id', postValidate, controller.changePost);
+router.put('/:id', postValidate, asyncHandler(controller.changePost));
 
 // Delete a post
-router.delete('/:id', controller.deletePost);
+router.delete('/:id', asyncHandler(controller.deletePost));
 
 module.exports = router;
